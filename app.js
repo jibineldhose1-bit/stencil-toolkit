b0152d42406782e0fb8c6185d2be5d02daab6ccb
jibineldhose1-bit <jibineldhose1@gmail.com>
2026-07-22 12:55:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -599,3 +599,15 @@ const inner = adaptiveErode(
 drawDots(inner);
 
 };
+
+downloadBtn.onclick = function () {
+
+    const link = document.createElement("a");
+
+    link.download = "stencil.png";
+
+    link.href = canvas.toDataURL("image/png");
+
+    link.click();
+
+};
