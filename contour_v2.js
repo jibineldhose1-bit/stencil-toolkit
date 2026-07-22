9241d4877c0cab3feda10dd74d06531ebb680f3c
jibineldhose1-bit <jibineldhose1@gmail.com>
2026-07-22 19:58:43 +0530

    Update contour_v2.js

--- contour_v2.js
+++ contour_v2.js
@@ -29,27 +29,23 @@ function traceContours(mask) {
 
     for (let i = 0; i < contours.size(); i++) {
 
-    const contour = contours.get(i);
-    const points = [];
+        const contour = contours.get(i);
+        const points = [];
 
-    for (let j = 0; j < contour.rows; j++) {
+        for (let j = 0; j < contour.rows; j++) {
 
-        points.push({
-            x: contour.intPtr(j, 0)[0],
-            y: contour.intPtr(j, 0)[1]
-        });
+            points.push({
+                x: contour.intPtr(j, 0)[0],
+                y: contour.intPtr(j, 0)[1]
+            });
 
-    }
+        }
 
-    if (points.length > 2) {
-        result.push(points);
-    }
+        if (points.length > 2) {
+            result.push(points);
+        }
 
-    contour.delete();
-
-}
-
-contour.delete();
+        contour.delete();
     }
 
     contours.delete();
