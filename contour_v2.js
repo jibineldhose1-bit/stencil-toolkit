098d688da283b2e82ee27c306eea88dd14b1c6c1
jibineldhose1-bit <jibineldhose1@gmail.com>
2026-07-22 19:10:13 +0530

    Update contour_v2.js

--- contour_v2.js
+++ contour_v2.js
@@ -30,22 +30,30 @@ function traceContours(mask) {
     for (let i = 0; i < contours.size(); i++) {
 
         const contour = contours.get(i);
-        const points = [];
+const approx = new cv.Mat();
 
-        for (let j = 0; j < contour.rows; j++) {
+cv.approxPolyDP(
+    contour,
+    approx,
+    1.5,   // smoothing amount
+    true
+);
 
-            points.push({
-                x: contour.intPtr(j, 0)[0],
-                y: contour.intPtr(j, 0)[1]
-            });
+const points = [];
 
-        }
+for (let j = 0; j < approx.rows; j++) {
+    points.push({
+        x: approx.intPtr(j, 0)[0],
+        y: approx.intPtr(j, 0)[1]
+    });
+}
 
-        if (points.length > 2) {
-            result.push(points);
-        }
+if (points.length > 2) {
+    result.push(points);
+}
 
-        contour.delete();
+approx.delete();
+contour.delete();
     }
 
     contours.delete();
