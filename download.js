055e19daa41a5686bcc59f3e5c5e636ca321b4b9
jibineldhose1-bit <jibineldhose1@gmail.com>
2026-07-22 14:42:25 +0530

    Update download.js

--- download.js
+++ download.js
@@ -1,1 +1,12 @@
 
+downloadBtn.onclick = function () {
+
+    const link = document.createElement("a");
+
+    link.download = "stencil.png";
+
+    link.href = canvas.toDataURL("image/png");
+
+    link.click();
+
+};
